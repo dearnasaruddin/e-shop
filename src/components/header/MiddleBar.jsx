5fe2c30da8b0b542dea38f0a35174f332f668243
dearnasaruddin <dearnasaruddin@gmail.com>
2026-01-14 13:00:50 +0600

    navbar mobile responsive color issue fixed

--- src/components/header/MiddleBar.jsx
+++ src/components/header/MiddleBar.jsx
@@ -48,25 +48,25 @@ const MiddleBar = () => {
   }
 
   return (
-    // <div className='sticky top-0 left-0 bg-white shadow-lg z-50'>
-    <div>
+    <div className='max-sm:sticky sm:static max-sm:top-0 max-sm:left-0 max-sm:bg-secondary sm:bg-white shadow-lg max-sm:z-50'>
       <Container>
-        <div className="flex justify-between items-center py-2 md:py-6 font-montserrat font-normal text-sm leading-5 text-secondary">
+        <div className="flex justify-between items-center py-3.5 sm:py-2 md:py-6 font-montserrat font-normal text-sm leading-5 max-sm:text-white sm:text-secondary">
           {/* ======= Logo ======= */}
           <div>
-            <Link to={'/'}><img src="/logo.svg" alt="logo.svg" /></Link>
+            <Link to={'/'} className='max-sm:hidden' ><img src="/logo.svg" alt="logo.svg" /></Link>
+            <Link to={'/'} className='sm:hidden' ><img src="/whiteLogo.svg" alt="whiteLogo.svg" /></Link>
           </div>
 
-          <div className='flex items-center gap-x-4 md:gap-x-12 '>
+          <div className='flex items-center gap-x-4 md:gap-x-12'>
             {/* ======= Bar ======= */}
             <span onClick={handleNavSideBar} className='inline-block sm:hidden cursor-pointer'>
               <HiBars3 className='text-[34px]' />
             </span>
 
             {/* ======= Input ======= */}
-            <div className={`${activeSearch && 'w-full sm:w-auto absolute sm:static top-20 md:top-0 left-1/2 md:left-0  -translate-x-1/2 sm:translate-0 z-10 px-2.5 sm:px-0 bg-white sm:bg-transparent'}`}>
+            <div className={`${activeSearch && 'w-full sm:w-auto absolute top-0 left-1/2 md:left-0  -translate-x-1/2 sm:translate-0 z-10 px-2.5 sm:px-0 max-sm:py-2 bg-white sm:bg-transparent'}`}>
               <div ref={searchRef} className={`${activeSearch ? 'w-full sm:w-auto' : 'relative'}`}>
-                <input className={`sm:w-full md:w-83 border border-[#979797] py-3 md:py-4.5 pl-4 pr-10 md:px-6 rounded-[10px] ${activeSearch ? 'inline-block w-full' : 'hidden'} sm:block`} placeholder='Search Products ...' type="text" name="" id="" />
+                <input className={`sm:w-full md:w-83 border border-[#979797] py-3 md:py-4.5 pl-4 pr-10 md:px-6 rounded-[10px] ${activeSearch ? 'inline-block w-full max-sm:shadow-2xl max-sm:text-secondary' : 'hidden'} sm:block`} placeholder='Search Products ...' type="text" />
                 <IoSearchOutline onClick={handleSearch} className={`${activeSearch ? 'absolute -translate-y-1/2 right-6 sm:right-0 pb-1 sm:pb-0' : 'right-0'} sm:absolute top-1/2 sm:-translate-1/2 md:right-3.5 text-3xl sm:text-2xl md:text-lg mt-0.5 sm:mt-0 cursor-pointer`} />
               </div>
             </div>
@@ -74,8 +74,11 @@ const MiddleBar = () => {
             <div className='flex items-center gap-x-3 sm:gap-x-6 md:gap-x-22.5'>
               {/* ======= Cart ======= */}
               <Link to={'/cart'} className='flex items-center gap-x-3 md:gap-x-5 relative after:absolute sm:after:w-[1px] sm:after:h-8 after:top-1/2 after:-right-3 md:after:-right-11 after:-translate-y-1/2 after:translate-x-1/2 after:bg-[#979797] cursor-pointer'>
-                <div>
+                <div className='max-sm:hidden'>
                   <CartIcon />
+                </div>
+                <div className='sm:hidden'>
+                  <CartIcon shapeColor='white' />
                 </div>
                 <div className='hidden sm:block'>
                   <p className='text-base leading-6'>Cart</p>
